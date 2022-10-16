7183fe8c20507cf4791e6516925ce45b31dc590a
UTRS7 <94729239+14221624@users.noreply.github.com>
2022-10-16 20:36:35 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -19,4 +19,54 @@ function take_snapshot()
 }
 
 console.log('ml5 version: ' , ml5.version);
-classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/R-aY7sHtL/model.json",modelLoaded);
+classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/XhWpMT41U/model.json",modelLoaded);
+
+function modelLoaded()
+{
+    console.log('Model Loaded!');
+}
+
+function check()
+{
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results)
+{
+     if (error)
+     {
+        console.error(error);
+     } else
+     {
+        console.log(results);
+        document.getElementById("result_emotion_name").innerHTML = results[0].label;
+
+        prediction_1 = results[0].label;
+        prediction_2 = results[1].label;
+        speak();
+
+        if(results[0].label == "perfect")
+        {
+            document.getElementById("update_emoji").innerHTML = "👌🏻";
+        }
+        if(results[0].label == "victory")
+        {
+            document.getElementById("update_emoji").innerHTML = "✌🏻";
+        }
+        if(results[0].label == "swag")
+        {
+            document.getElementById("update_emoji").innerHTML = "🤘🏻";
+        }
+     }
+}
+
+function speak()
+{
+    var synth = window.speechSynthesis;
+    speak_data_1 = "The prediction is " + prediction_1;
+    
+    var utterThis = new SpeechSynthesisUtterance(speak_data_1);
+    utterThis.rate = 1.3;
+    synth.speak(utterThis);
+}
